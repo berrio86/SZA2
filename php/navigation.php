d7f9d2e30a91f8d2e46af40cf413655cef2745cc
Inaki Berriotxoa <berriotxoa86@gmail.com>
2016-11-29 17:07:01 +0100

    aldaketa batzuk xhtml-n onartzeko

--- php/navigation.php
+++ php/navigation.php
@@ -1,12 +1,12 @@
 <div id='page-wrap'>
-	<header class='main' id='h1'>
-		<a href="hasiera.php"><img class="logo" src="irudiak/quiz-logo.png"></a>	
-		<a href="logout.php"><img title="Saioa amaitu" class="botoia"  src="irudiak/logout-icon.png"></a>
+	<header>
+		<a href="hasiera.php"><img class="logo" src="irudiak/quiz-logo.png" alt="Aplikazio logo" /></a>	
+		<a href="logout.php"><img title="Saioa amaitu" class="botoia"  src="irudiak/logout-icon.png" alt="Log out botoia"/></a>
 		<?php
 			echo'<a href="#" class="datuak"><span><strong>Izena: </strong>'.$_SESSION['izena'].'<br/><strong>Emaila: </strong>'.$_SESSION['eposta'].'<br/><strong>Mota: </strong>'.$_SESSION['erabiltzaileMota'].'<br/></span></a>'; 
     	?>
     </header>
-	<nav class='main desktopSoilik' id='n1' role='navigation'>
+	<nav>
 	<?php
 	if($_SESSION['erabiltzaileMota'] == 'Administratzailea') {
 		echo ('<a href="hasiera.php"><span id="act-sel" class="act-sel">Datu basea kudeatu<div class="arrow-right"></div></span></a>');
