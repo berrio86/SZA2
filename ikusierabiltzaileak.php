3c93dea0409ec39367b9f3f9dc5ee2ef8ee55518
master <julenmg12@gmail.com>
2016-12-01 23:09:37 +0100

    ikusierabiltzaileak

--- ikusierabiltzaileak.php
+++ ikusierabiltzaileak.php
@@ -2,11 +2,22 @@
 session_start();
 include 'php/header.php';
 //horrela jarrita dago javascript funtzioak sartu behar badira head atalean.
-echo('<script type="text/javascript" >
-function erabEzabatu()
+?><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>
+
+$(document).ready(function(){
+$("table tr").click( function () {
+	$(this).addClass('selected').siblings().removeClass('selected');
+	var eposta=$(this).find("td:first").html();
+	var izena=$(this).find("td:last").html();
+	var id= $(this).text();
+        alert(id);
+        } );
+});
 </script>
+
 </head>
-		<body>');
+		<body><?php
 include 'php/navigation.php';
 ?>
 <section class="main" id="s1">
